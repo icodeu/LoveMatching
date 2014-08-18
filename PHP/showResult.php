bec5f5929f5b12b8c1d88ae97b74970d4468fe20
icodeu <icodeyou@sina.com>
2014-08-18 23:31:55 +0800

    showResult.php

--- PHP/showResult.php
+++ PHP/showResult.php
@@ -2,19 +2,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
 <title>showResult</title>
 </head>
 
 <body>
-<?php
-	include "mysql_connect.php";
-	
-	$result = mysql_query("SELECT * FROM success");
-	while ($row = mysql_fetch_array($result))
-	{
-		echo $row['tel1']."            ".$row['tel2'];
-		echo "</br>";
-	}
-?>
+    <?php
+        include "mysql_connect.php";
+        $result = mysql_query("SELECT * FROM success");
+	?> 
+    <div class="container" style="width:300px">
+    	<h1>匹配成功列表</h1>
+    <?php
+	    echo '<table class="table table-hover">';
+        while ($row = mysql_fetch_array($result))
+        {
+			echo '<tr>';
+            echo '<td>'.$row['tel1'].'</td>'.'<td>'.$row['tel2'].'</td>';
+            echo "</tr>";
+        }
+	?>
+    </div>
 </body>
+
+
+
+<script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </html>
