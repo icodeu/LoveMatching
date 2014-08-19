f25d870ac8283131ffda86552f1372fdc19b23f4
icodeu <icodeyou@sina.com>
2014-08-19 15:51:30 +0800

    修改了index.php   新加了sms.php

--- PHP/index.php
+++ PHP/index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
 <title>登陆</title>
 </head>
 <script>
@@ -26,17 +28,41 @@ if(t1.length != 11 || t2.length != 11){
 	else return false;
 }
 </script>
-<body>
-	<?php echo $showtime=date("Y-m-d H:i:s");?>
-	<form method="post" action="doaction.php"  onsubmit="return doCheck()" name="regist">
-		<ul class="login">
-			<li class="l_tit">你的手机号</li>
-			<li ><input type="text"  name="number1" placeholder="你的手机号" class="login_input user_icon"></li>
-			<li class="l_tit">他（她）的手机号</li>
-			<li class="mb_10"><input type="password" name="number2"  placeholder="ta的手机号" class="login_input user_icon"></li>			
-			<li><input type="submit" value="提交" ></li>
-		</ul>
-		</form>
+<body background="images/bg.png">
+	<?php $showtime=date("Y-m-d H:i:s");?>
+    <div class="container">
+     	<img src="images/header.png" width="300">
+    	<form class="form-horizontal" role="form"  method="post" action="doaction.php"  onsubmit="return doCheck()" name="regist">
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-2 control-label"><p class="text-success">你的手机号</p></label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="inputEmail3" placeholder="你的手机号" name="number1">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputPassword3" class="col-sm-2 control-label"><p class="text-success">你的手机号</p></label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="inputPassword3" placeholder="TA的手机号" name="number2">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <center><button type="submit" class="btn btn-default btn-success">提交匹配</button></center>
+            </div>
+          </div>
+   		</form>
+    </div>
 		
 </body>
+<script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </html>
+
+<!--<form method="post" action="doaction.php"  onsubmit="return doCheck()" name="regist">
+            你的手机号
+			<input type="text"  name="number1" placeholder="你的手机号" class="login_input user_icon">
+			TA的手机号
+			<input type="password" name="number2"  placeholder="TA的手机号" class="login_input user_icon">		
+			<input type="submit" value="提交" >
+		
+	</form>-->
