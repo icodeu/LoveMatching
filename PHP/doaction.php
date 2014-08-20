e1e4b14e564fc71fe5f25801912bb21b656ca969
icodeu <icodeyou@sina.com>
2014-08-21 00:14:55 +0800

    Done

--- PHP/doaction.php
+++ PHP/doaction.php
@@ -16,6 +16,34 @@ $number1 = $_POST["number1"];
 $number2 = $_POST["number2"];
 $sql="insert into love(me,you,date,result) VALUES('$number1','$number2','$time','$false')";
 mysql_query($sql) or die(mysql_error());
+
+//给number2发短信
+function Post($data, $target) {
+    $url_info = parse_url($target);
+    $httpheader = "POST " . $url_info['path'] . " HTTP/1.0\r\n";
+    $httpheader .= "Host:" . $url_info['host'] . "\r\n";
+    $httpheader .= "Content-Type:application/x-www-form-urlencoded\r\n";
+    $httpheader .= "Content-Length:" . strlen($data) . "\r\n";
+    $httpheader .= "Connection:close\r\n\r\n";
+    //$httpheader .= "Connection:Keep-Alive\r\n\r\n";
+    $httpheader .= $data;
+
+    $fd = fsockopen($url_info['host'], 80);
+    fwrite($fd, $httpheader);
+    $gets = "";
+    while(!feof($fd)) {
+        $gets .= fread($fd, 128);
+    }
+    fclose($fd);
+    return $gets;
+}
+
+	$target = "http://sms.106jiekou.com/utf8/sms.aspx";
+	//替换成自己的测试账号,参数顺序和wenservice对应
+	$post_data = "account=icodeyou&password=qinaidaqiqi&mobile=".$number2."&content=".rawurlencode("您的订单编码：【恭喜！有人说暗恋你，主动想捅破窗户纸~打开www.icodeyou.com即可查看O(∩_∩)O】。如需帮助请联系客服。");
+	echo $gets = Post($post_data, $target);
+	$return_code = substr($gets,strlen($gets)- 3,3);
+	echo "结果码".$return_code;
 ?>
 
 <script language='javascript' type='text/javascript'> 
