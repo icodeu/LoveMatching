b3d663fd7b593f531b73524d87a7ccc7511bf238
icodeu <icodeyou@sina.com>
2014-08-22 18:05:29 +0800

    Update mysql_connect.php

--- PHP/mysql_connect.php
+++ PHP/mysql_connect.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
-</head>
-
-<body>
-	<?php
-		$con = mysql_connect("localhost","root","");
-		mysql_query("SET NAMES 'utf8'"); 
-		mysql_query("SET CHARACTER SET utf8"); 
-		mysql_query("SET CHARACTER_SET_RESULTS=utf8'"); 
+<?php
+		$con = mysql_connect(SAE_MYSQL_HOST_M . ':' . SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS);
+		mysql_query("SET NAMES 'UTF8'"); 
+		mysql_query("SET CHARACTER SET UTF8"); 
+		mysql_query("SET CHARACTER_SET_RESULTS=UTF8'"); 
 		if (!$con)
 		{
 			die('Could not connect: ' . mysql_error());
 		}
-		mysql_select_db("mylove", $con);
-	?>
-</body>
-</html>
+		mysql_select_db(SAE_MYSQL_DB, $con);
+?>
